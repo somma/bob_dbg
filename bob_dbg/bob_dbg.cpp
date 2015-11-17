b8d9e6b64bd2426970f645f92c4f851a4d10dabc
somma <fixbrain@gmail.com>
2015-11-17 14:04:19 +0900

    initial commit.

---
+++ bob_dbg/bob_dbg.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file    main.cpp
+ * @brief   Debugger application's entry point.
+ *
+ * @author  Yonhgwhan, Noh (fixbrain@gmail.com)
+ * @date    2015.11.17 11:17 created.
+ * @copyright All rights reserved by Yonghwan, Noh.
+**/
+
+#include "stdafx.h"
+
+/// @brief
+int main()
+{
+    log("hello");
+    _getch();
+    return 0;
+}
+
---
+++ bob_dbg/debugger.h
@@ -0,0 +1,11 @@
+/**
+ * @file    debugger.h
+ * @brief   debugger core
+ *
+ * @author  Yonhgwhan, Noh (fixbrain@gmail.com)
+ * @date    2015.11.17 11:17 created.
+ * @copyright All rights reserved by Yonghwan, Noh.
+**/
+#pragma once
+
+
